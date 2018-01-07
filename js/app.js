fb76b9cbadab0616ff682bb793f0bc9c1c426b0e
andersp89 <anders@ptrading.dk>
2018-01-07 11:22:59 +0100

    Set marker by click on list item and get streetview photo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 /* Travel Planner - Udacity */
 
-(function() {
-	'use strict';
+// QUESTIONS:
+// * How could I put initMap and supporting functions in the ViewModel in the IIFE?
+
+//(function() {
+	//'use strict';
 
 	// Model
 	var locations = [
@@ -31,7 +34,7 @@
 			self.locationsList.push(new Marker(marker));
 		});
 
-		// Hide or show menu by click on hamburger icon
+		// Hide or show menu by clicking hamburger icon
 		self.showMenu = ko.observable(true);
 		self.ShowHideHamburgerMenu = function() {
 			if (self.showMenu() == true) {
@@ -40,20 +43,35 @@
 				self.showMenu(true);
 			}
 		};
+
+		// Set current location
+		self.currentLocation = ko.observable( self.locationsList()[0] );
+		self.setCurrentLocation = function(newLocation) {
+			// Initiate all variables needed, to create markers at map
+			self.currentLocation(newLocation);			
+			var title = self.currentLocation().title()
+			var id = getId(title);
+			var array = createMarkerArray();
+
+			var highlightedIcon = makeMarkerIcon('FF9933');
+			setIconOnMarker(array[id], highlightedIcon);
+			
+			var largeInfowindow = new google.maps.InfoWindow();
+			populateInfoWindow(array[id], largeInfowindow)	
+		};
 	};
 
 	ko.applyBindings(new ViewModel())
 
-})();
-
+//})();
 
 /* Google Maps API */
 
 var map;
 var markers = [];
 var markerSelected;
+var windowOpened;
 
-// QQ: How could I put initMap and supporting functions in the ViewModel in the IIFE?
 function initMap() {
 	// Constructor creates a new map - only center and zoom are required.
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -62,31 +80,19 @@ function initMap() {
 		mapTypeControl: false
 	});
 
-	// Markers, once stored in db
-	var locations = [
-		{title: 'Trifork', location: {lat: 56.153944, lng: 10.212539}},
-		{title: 'Köd Restaurant', location: {lat: 56.155178, lng: 10.209552}},
-		{title: 'Aros', location: {lat: 56.153919, lng: 10.199716}},
-		{title: 'Stående Pige', location: {lat: 56.152135, lng: 10.200845}},
-		{title: 'Den Gamle By', location: {lat: 56.158783, lng: 10.192115}},
-		{title: 'Latiner Kvarteret', location: {lat: 56.158775, lng: 10.210766}},
-		{title: 'Fitness World - Århus C.', location: {lat: 56.144076, lng: 10.199951}}
-	]
-
 	var largeInfowindow = new google.maps.InfoWindow();
 
 	// Create a "highlighted location" marker color for when the user
 	// click location.
 	var highlightedIcon = makeMarkerIcon('FF9933');
-	var markerSelected;
 
 	// The following group uses the location array to create an array of markers on initialize.
 	for (var i = 0; i < locations.length; i++) {
 		// Get the position from the location array.
 		var position = locations[i].location;
 		var title = locations[i].title;
+		
 		// Create a marker per location, and put into markers array.
-
 		var marker = new google.maps.Marker({
 			position: position,
 			title: title,
@@ -104,23 +110,90 @@ function initMap() {
 			setIconOnMarker(this, highlightedIcon)
 		})
 	};
+	createMarkerArray(markers);
 	showListings();
 };
+
+function setInfoWindow(marker, infowindow) {
+	// code here
+}
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
 function populateInfoWindow(marker, infowindow) {
-	// Check to make sure the infowindow is not already opened on this marker.
-	if (infowindow.marker != marker) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          // Clear the infowindow content to give the streetview time to load.
+          infowindow.setContent('');
+          infowindow.marker = marker;
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+          var streetViewService = new google.maps.StreetViewService();
+          var radius = 50;
+          // In case the status is OK, which means the pano was found, compute the
+          // position of the streetview image, then calculate the heading, then get a
+          // panorama from that and set the options
+          function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+              var nearStreetViewLocation = data.location.latLng;
+              var heading = google.maps.geometry.spherical.computeHeading(
+                nearStreetViewLocation, marker.position);
+                infowindow.setContent('<div id="infoWindowTitle">' + marker.title + '</div><div id="infoWindowPano"></div>');
+                var panoramaOptions = {
+                  position: nearStreetViewLocation,
+                  pov: {
+                    heading: heading,
+                    pitch: 30
+                  }
+                };
+              var panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('infoWindowPano'), panoramaOptions);
+            } else {
+              infowindow.setContent('<div>' + marker.title + '</div>' +
+                '<div>No Street View Found</div>');
+            }
+          }
+          // Use streetview service to get the closest streetview image within
+          // 50 meters of the markers position
+
+          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+          // Open the infowindow on the correct marker.
+          infowindow.open(map, marker);
+        }	
+
+	/*if (windowOpened == null && infowindow.marker != marker) {
 		infowindow.marker = marker;
 		infowindow.setContent('<div>' + marker.title + '</div>');
 		infowindow.open(map, marker);
-		// Make sure the marker property is cleared if the infowindow is closed.
-		infowindow.addListener('closeclick', function() {
-			infowindow.marker = null;
-		});
+		windowOpened = infowindow;
+
+	} else {
+		windowOpened.close();
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+		windowOpened = infowindow;
+	};*/
+};
+
+var createMarkerArray = (function(array) {
+	return function() {
+		return array;
 	};
+})(markers);
+
+function getId(title) {	
+	// Create array of titles
+	var titleArray = [];
+	for (var i = 0; i < locations.length; i++) {
+		titleArray.push(locations[i].title)
+	}
+	return titleArray.findIndex(function(search) {
+		return search == title;
+	});
 };
 
 function setIconOnMarker(marker, highlightedIcon) {
@@ -134,6 +207,7 @@ function setIconOnMarker(marker, highlightedIcon) {
 		markerSelected = marker;
 	}
 };
+
 
 // This function will loop through the markers array and display them all.
 function showListings() {
